622328f803704a69f8e3bd48581ef2425b06021f
dafer-t480 <dafernandezperez@gmail.com>
2025-08-23 17:09:11 +0200

    fix: remove nvim config, changed gitsubmodules

--- configs/nvim/lua/daniel/mason.lua
+++
@@ -1,9 +0,0 @@
--- ~/.config/nvim/lua/daniel/mason.lua
-require("mason").setup()
-
--- Load servers table from lspconfig.lua
-local servers = require("daniel.lspconfig")
-
-require("mason-lspconfig").setup({
-    ensure_installed = vim.tbl_keys(servers),
-})
