efd6a8422aebf8cac4b783ce925f16f1fbb1775b
Daniel Pérez Fernández <daniel.perezf@energy.sener>
2026-04-14 14:08:50 +0200

    chore: reorganize windows scripts

--- scripts/windows/ape.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-set "_MARKER=%TEMP%\_venv_activate_path.txt"
-if exist "%_MARKER%" del "%_MARKER%"
-"%~dp0ape-core.exe" %*
-if exist "%_MARKER%" (
-    set /p "_VENV_PATH="<"%_MARKER%"
-    del "%_MARKER%"
-    call "%_VENV_PATH%\Scripts\activate.bat"
-)
